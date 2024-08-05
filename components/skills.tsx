7ebd3818795e533bd8a97d287b8c2a3101917f60
Hadi <hadihaz1999@gmail.com>
2024-08-05 21:43:49 +0330

    edit skills component

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 import { FaCss3Alt, FaGit, FaHtml5, FaReact } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiNextdotjs, SiPython, SiTailwindcss, SiTypescript } from "react-icons/si";
+import {
+  SiNextdotjs,
+  SiPython,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
 const skills = [
   {
     name: "React",
@@ -17,7 +21,7 @@ const skills = [
   },
   {
     name: "TypeScript",
-    icon: <SiTypescript  size={20} color="#0040ff" />,
+    icon: <SiTypescript size={20} color="#1f57ff" />,
     color: "#0040ff",
   },
   {
@@ -27,7 +31,7 @@ const skills = [
   },
   {
     name: "CSS",
-    icon: <FaCss3Alt size={20} color="#3e22f5" />,
+    icon: <FaCss3Alt size={20} color="#1f57ff" />,
     color: "#3e22f5",
   },
   {
@@ -37,7 +41,7 @@ const skills = [
   },
   {
     name: "Git",
-    icon: <FaGit size={20} color="#ff2200" />,
+    icon: <FaGit size={20} color="#ed392f" />,
     color: "#ff2200",
   },
   {
@@ -47,55 +51,42 @@ const skills = [
   },
   {
     name: "Python",
-    icon: <SiPython size={20} color="#ffae00" />,
+    icon: <SiPython size={20} color="#ffc956" />,
     color: "#ffae00",
   },
 ];
 export default function Skills() {
   const [isMobile, setIsMobile] = useState(false);
-
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); 
+      setIsMobile(window.innerWidth < 768);
     };
-
-    handleResize(); 
-
-    window.addEventListener("resize", handleResize); 
-
+    handleResize();
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener("resize", handleResize); // حذف رویداد برای جلوگیری از حافظه نگهداری ناخواسته
+      window.removeEventListener("resize", handleResize); 
     };
   }, []);
   return (
-    <div className="w-full max-w-screen-2xl mx-auto px-4 md:px-28 pt-12">
-      <motion.div
-        initial={{ opacity: 0, x: +300 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.5 }}
-        className="flex flex-col justify-center items-start "
-      >
-        <h1 className="font-bold text-3xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-green-700 border-green-400 border-b-2 w-full p-2">{`<Skills />`}</h1>
-
-        <ul className="md:p-10 p-4 md:font-bold md:text-2xl flex gap-2 md:gap-4 flex-wrap">
+    <div className="w-full max-w-screen-2xl mx-auto px-4 lg:px-28 pt-12">
+      <div className="flex flex-col justify-center items-start ">
+        <h1 className="my-2 w-full p-2 pl-6 md:pl-12 font-bold text-3xl md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-green-700 border-green-400 border-b-2 ">{`Skills `}</h1>
+        <ul className="md:p-6 p-2 md:font-bold md:text-2xl flex justify-center gap-2 md:gap-4 flex-wrap ">
           {skills.map((skill) => (
-            <motion.li
-              initial={{ opacity: 0, x: -500 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5 }}
+            <li
               key={skill.name}
               className={`
-              text-[${skill.color}]  border-[${skill.color}] text-neutral-400 shadow-2xl rounded-xl  flex justify-center items-center`}
+              text-[${skill.color}] border-[${skill.color}] text-neutral-400 drop-shadow-2xl rounded-xl flex justify-center items-center`}
             >
-              <span className="cursor-pointer hover:text-neutral-100 hover:scale-105 duration-200 flex gap-2 items-center px-3 md:px-6 py-1 md:py-3 bg-neutral-900 rounded-xl">
+              <span  className="  text-xs cursor-pointer hover:text-neutral-100 hover:scale-105 duration-200 flex flex-col  gap-2 items-center px-3 md:px-6 py-1 md:py-3 bg--neutral-800 rounded-xl">
                 {/* {skill.icon} */}
-                {React.cloneElement(skill.icon, { size: isMobile ? 20 : 40 })} 
+                  {React.cloneElement(skill.icon, { size: isMobile ? 40 : 40 })}
                 {`<${skill.name} />`}
               </span>
-            </motion.li>
+            </li>
           ))}
         </ul>
-      </motion.div>
+      </div>
     </div>
   );
 }
